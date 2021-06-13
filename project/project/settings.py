d1cd7ea18469861ced0b660fab04d138c15a4e99
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-06-13 16:36:31 +0600

    added design temp

--- project/project/settings.py
+++ project/project/settings.py
@@ -118,11 +118,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR / 'static'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATIC_ROOT = "/static"
+
+STATICFILES_DIRS=[
+    BASE_DIR/ 'static'
+]
+
+MEDIA_ROOT='static/images'
